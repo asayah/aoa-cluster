fb4d3b9dbe7b4fe55d1d50c92b1bea33e557d0e8
asayah <adam@solo.io>
2022-08-14 20:42:03 -0700

    adding some check changes

--- deploy.sh
+++ deploy.sh
@@ -22,11 +22,12 @@ pending_changes=`git status -s`
 pending_commits= `git --no-pager log  --not --remotes --decorate=short --pretty=oneline -n1`
 
 
-if [[ -z $pending_changes || -z $pending_commits ]]; then
-  echo -e "\xE2\x9D\x8C" "- Your local branch is not in sync with the remote repository"
-else  
+if [ "$pending_changes-$pending_commits" = "-" ]; then
   echo -e "\xE2\x9C\x94" "- Your local branch is in sync with the remote repository"
+else 
+  echo -e "\xE2\x9D\x8C" "- Your local branch is not in sync with the remote repository"
 fi
+
 echo "----------------------------------------"
 
 
