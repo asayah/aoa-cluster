4a293fa5c4224d35a628c28fc53b958b6a4199f2
asayah <adam@solo.io>
2022-08-15 08:20:07 -0700

    checking for remote changes

--- deploy.sh
+++ deploy.sh
@@ -20,9 +20,10 @@ gloo_mesh_version="2.0.9"
 
 pending_changes=`git status -s`
 pending_commits=`git --no-pager log $branch --not --remotes --decorate=short --pretty=oneline -n1`
+remote_commits=`git --no-pager log origin/$branch --not --remotes --decorate=short --pretty=oneline -n1`
 
 
-if [ "$pending_changes-$pending_commits" = "-" ]; then
+if [ "$pending_changes-$pending_commits-$remote_commits" = "--" ]; then
   echo -e "\xE2\x9C\x94" "- Your local branch is in sync with the remote repository"
 else 
   echo -e "\xE2\x9D\x8C" "- Your local branch is not in sync with the remote repository"
